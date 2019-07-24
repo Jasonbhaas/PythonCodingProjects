40a5ede17866d2d15ffdd5840d6c8636b77b64a4
Jason Haas <Jasonbhaas@gmail.com>
2019-07-24 15:52:07 -0700

    fixed max points solution

--- Misc/deleteAndEarn.py
+++ Misc/deleteAndEarn.py
@@ -15,8 +15,10 @@ def maxPoints(elements):
         if prev_num != num:
             sum_so_far = 0
         sum_so_far += num
-        if prev_num - num > 3 and helper_cache[num + 3] == 0:
-            helper_cache[num + 3] = max_so_far
+        # to ensure that if/when there's a gap in sequential numbers,
+        # that the points carry through
+        if prev_num - num > 2 and helper_cache[num + 2] == 0:
+            helper_cache[num + 2] = max_so_far
 
         helper_cache[num] = sum_so_far + \
             max(helper_cache[num + 2], helper_cache[num + 3])
